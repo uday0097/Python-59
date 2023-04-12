67952e4424fe48a7036d99899e017daf90307215
shivani121794 <shivani121794@gmail.com>
2023-04-12 08:04:21 +0530

    update comments in program_3.py

--- april_revision/program_3.py
+++ april_revision/program_3.py
@@ -1,4 +1,5 @@
 # accept a string and a char from user and check if char is present in str
-user_input_string=input("str:")
-user_input_character=input("char:")
-print(user_input_string.find(user_input_character))
+user_input_string=input("str:")  # get string from user
+user_input_character=input("char:") # get char from user
+print(user_input_string.find(user_input_character)) # returns the first index
+                                                    # if char is present else -1
