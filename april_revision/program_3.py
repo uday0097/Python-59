5f2f21ba3b9d6c9416d33e3bd583bf44d55ae8ca
shivani121794 <shivani121794@gmail.com>
2023-04-12 07:45:52 +0530

    update code in program_3.py

--- april_revision/program_3.py
+++ april_revision/program_3.py
@@ -1,1 +1,4 @@
 # accept a string and a char from user and check if char is present in str
+user_input_string=input("str:")
+user_input_character=input("char:")
+print(user_input_string.find(user_input_character))
