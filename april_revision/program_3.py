bc95748d706662bdbdbe21d2f9f87cbe75657712
Tinku-13 <tinkuaglecha981@gmail.com>
2023-04-13 21:31:01 +0530

    add program_3.py

--- april_revision/program_3.py
+++ april_revision/program_3.py
@@ -3,3 +3,4 @@ user_input_string=input("str:")  # get string from user
 user_input_character=input("char:") # get char from user
 print(user_input_string.find(user_input_character)) # returns the first index
                                                     # if char is present else -1
+#accept a string and a char from user and check if char is present in str
